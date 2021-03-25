89a2b47c20acd1e9fc48070a02d1a13d6c10796b
Mark Oborne <markoborne1@gmail.com>
2021-03-25 22:59:52 +0000

    started rework

--- src/ISSAPI.py
+++ src/ISSAPI.py
@@ -1,19 +1,15 @@
 import requests
 
+def position():
+    posRequest = requests.get("http://api.open-notify.org/iss-now.json")
+    
+    if posRequest.status_code == 200:  # anything other than 200 can result in unknown data being printed so an exception is set
+        posReply = posRequest.json()
 
-class ISS:
-
-    def __init__(self):
-        self.posRequest = requests.get("http://api.open-notify.org/iss-now.json")  # API for positional data of the ISS
-
-    def position(self):
-        if self.posRequest.status_code == 200:  # anything other than 200 can result in unknown data being printed so an exception is set
-            posReply = self.posRequest.json()
-
-            if type(posReply) == dict:
-                return "Latitude:", posReply['iss_position']['latitude'] + ", Longitude:", posReply['iss_position'][
-                    'longitude']
-            else:
-                return "Error class type:", type(posReply)
+        if type(posReply) == dict:
+            return "Latitude:", posReply['iss_position']['latitude'] + ", Longitude:", posReply['iss_position'][
+                'longitude']
         else:
-            return "Error:", self.posRequest.status_code  # prints the exact error code for debugging purposes
+            return "Error class type:", type(posReply)
+    else:
+        return "Error:", posRequest.status_code  # prints the exact error code for debugging purposes
--- src/client.py
+++ src/client.py
@@ -1,7 +1,7 @@
 import sys
 import discord
 import random
-from src.ISSAPI import ISS
+from src.ISSAPI import position
 
 random.seed()
 
@@ -16,85 +16,94 @@ def readServerToken():
         lines = file.readlines()
         return lines[1].strip()
 
-class client(discord.Client): # (maybe in constructor) ping discord.com?
-    ISSdata = ISS()
-    clientID = readServerToken()
-    messageCount = 0
-    prevAuthor = ""
-    commandList = "!users\n" \
-                  "!tealc\n" \
-                  "!eu4wiki\n" \
-                  "!run\n" \
-                  "!rollthedice\n" \
-                  "!whereistheiss"
-    botCheck = bool
+async def run_file(message):
+    if message.content.endswith(".py"):
+        await message.channel.send("Indentation error")
+    elif message.content.endswith(".cpp"):
+        await message.channel.send(f"""Missing semi-colon on line: {random.randint(0, 100)}""")
+    elif message.content.endswith(".c"):
+        await message.channel.send("Segmentation fault")
+    else:
+        await message.channel.send("What am I supposed to do with this")
+
+async def dice_roll(message):
+    await message.channel.send(f"""The d20 lands on: " + {random.randint(1, 20)}""")
+
+async def iss_pos(message):
+    await message.channel.send(position())
+
+class client(discord.Client):
+    def __init__(self):
+        super().__init__()
+        for key in self.text_commands.keys():
+            self.command_list += '\n'
+            self.command_list += key
+        for key in self.adv_commands.keys():
+            self.command_list += '\n'
+            self.command_list += key
+            
+        with open("textComands.txt") as file:
+            for line in file:
+                (key, value) = line.split()
+                self.text_commands.update({key: value})
+        
+    async def user_count(self, message):
+        await message.channel.send(f"""# of Members {self.get_guild(int(self.client_ID)).member_count}""")
+    
+    async def help(self, message):
+        await message.channel.send(self.command_list)
+        
+    client_ID = readServerToken()
+    command_prefix = '!'
+    message_count = 0
+    prev_author = ""
+    text_commands = {"!tealc": "Indeed.",
+                     "": ""}
+
+    adv_commands = {"!users": user_count,
+                    "!run": run_file,
+                    "!rollthedice": dice_roll,
+                    "!whereistheiss": iss_pos,
+                    "!help": help}
+    
+    command_list = "Commands:"
+    has_other_bot = bool
 
     def getMembers(self):
-        return self.get_guild(int(self.clientID)).members
+        return self.get_guild(int(self.client_ID)).members
 
     async def on_ready(self):
-        print("Bot logged in as {0.user}".format(self)) # confirmation that bot has connected and is ready
-        for x in self.get_guild(int(self.clientID)).members:
-            if x.bot: # checks if there is another bot on the server to help with commands clashing
-                self.botCheck = True
+        print("Bot logged in as {0.user}".format(self))  # confirmation that bot has connected and is ready
+        for x in self.get_guild(int(self.client_ID)).members:
+            if x.bot:  # checks if there is another bot on the server to help with commands clashing
+                self.has_other_bot = True
+                print("Another bot has been detected on the server.")
             else:
-                self.botCheck = False
+                self.has_other_bot = False
 
-    async def on_message(self, message): # is called every time a message is sent by any user
+    async def get_command(self, message):
+        if message.content in self.text_commands:
+            await message.channel.send(self.text_commands[message.content])
+        elif message.content in self.adv_commands:
+            self.adv_commands[message.content](message)
+        elif message.content.startswith(self.command_prefix):
+            if not self.has_other_bot:
+                await message.channel.send("Command not found")
+
+    async def on_message(self, message):  # is called every time a message is sent by any user
         if message.author == self.user:
-            return # stops the bot from replying to itself
+            return  # stops the bot from replying to itself
         print("Message from {0.author}: {0.content} in channel {0.channel}".format(message))
 
-        if message.author == self.prevAuthor:
-            self.messageCount += 1
+        if message.author == self.prev_author:
+            self.message_count += 1
         else:
-            self.prevAuthor = message.author
+            self.prev_author = message.author
 
-        if self.messageCount >= 5:
+        if self.message_count >= 5:
             await message.channel.send("That's a lot of messages in a row man")
-
-        if message.content == "!users":
-            await message.channel.send(f"""# of Members {self.get_guild(int(self.clientID)).member_count}""")
-
-        elif message.content == "!chatmode":
-            await message.delete()
-            while True:
-                command = input("Type command:")
-                if command == "break":
-                    break
-                else:
-                    await message.channel.send(f"""{command}""")
-
-        elif message.content == "!tealc":
-            await message.channel.send("Indeed.")
-
-        elif message.content == "!eu4wiki":
-            await message.channel.send("https://eu4.paradoxwikis.com/Europa_Universalis_4_Wiki")
-
-        elif message.content.startswith("!run"):
-            if message.content.endswith(".py"):
-                await message.channel.send("Indentation error")
-            elif message.content.endswith(".cpp"):
-                await message.channel.send(f"""Missing semi-colon on line: {random.randint(0, 100)}""")
-            elif message.content.endswith(".c"):
-                await message.channel.send("Segmentation fault")
-            else:
-                await message.channel.send("What am I supposed to do with this")
-
-        elif message.content == "!help":
-            await message.channel.send("Commands:\n" + self.commandList)
-
-        elif message.content == "!rollthedice":
-            await message.channel.send("The d20 lands on: " + random.randint(1, 20))
-
-        elif message.content == "!whereistheiss":
-            await message.channel.send(self.ISSdata.position())
-
-        elif message.content.startswith("!"): # disabled if another bot is on the server
-            await message.channel.send("Error: Unknown command")
 
 #    async def on_member_join(self, member):
 #        for channel in member.server.channels:
 #            if channel == "general":
 #                await client.send_message(f"""Welcome to the server {member.mention}, enjoy your stay!""")
-
